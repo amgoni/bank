e80d7ecb7a493e682b8940dd86b58077d63b2661
Amin Mohammed Goni <amgoni.ug@atbu.edu.ng>
2023-05-07 23:46:54 +0100

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`font-poppins font-medium text-[18px] text-primary outline-none py-4 px-6 bg-blue-gradient ${styles} rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
